f3ab86987576c68f46d7bcbb13ec720836e8ac27
Berkay Tuncel <berkaytncl98@gmail.com>
2023-11-22 14:50:38 +0300

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Berkay Tuncel on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
