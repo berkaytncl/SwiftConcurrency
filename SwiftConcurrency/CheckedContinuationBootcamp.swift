529e7b8e6dd708be43d8df7c213d87bf043e1e46
Berkay Tuncel <berkaytncl98@gmail.com>
2023-12-05 17:33:15 +0300

    Add notes and examples for the Continuations

---
+++ SwiftConcurrency/CheckedContinuationBootcamp.swift
@@ -0,0 +1,98 @@
+//
+//  CheckedContinuationBootcamp.swift
+//  SwiftConcurrency
+//
+//  Created by Berkay Tuncel on 5.12.2023.
+//
+
+import SwiftUI
+
+final class CheckedContinuationBootcampNetworkManager {
+    
+    func getData(url: URL) async throws -> Data {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return data
+        } catch {
+            throw error
+        }
+    }
+    
+    func getData2(url: URL) async throws -> Data {
+        try await withCheckedThrowingContinuation { continuation in
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data {
+                    continuation.resume(returning: data)
+                } else if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(throwing: URLError(.badURL))
+                }
+            }
+            .resume()
+        }
+    }
+    
+    private func getHeartImageFromDatabase(completionHandler: @escaping (_ image: UIImage) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            completionHandler(UIImage(systemName: "heart.fill")!)
+        }
+    }
+    
+    func getHeartImageFromDatabase() async -> UIImage {
+        await withCheckedContinuation { continuation in
+            getHeartImageFromDatabase { image in
+                continuation.resume(returning: image)
+            }
+        }
+    }
+}
+
+final class CheckedContinuationBootcampViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    private let networkManager = CheckedContinuationBootcampNetworkManager()
+    
+    func getImage() async {
+        guard let url = URL(string: "https://picsum.photos/300") else { return }
+        do {
+            let data = try await networkManager.getData2(url: url)
+            
+            if let image = UIImage(data: data) {
+                await MainActor.run {
+                    self.image = image
+                }
+            }
+        } catch {
+            print(error.localizedDescription )
+        }
+    }
+    
+    func getHeartImage() async {
+        self.image = await networkManager.getHeartImageFromDatabase()
+    }
+}
+
+struct CheckedContinuationBootcamp: View {
+    
+    @StateObject private var viewModel = CheckedContinuationBootcampViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .task {
+//            await viewModel.getImage()
+            await viewModel.getHeartImage()
+        }
+    }
+}
+
+#Preview {
+    CheckedContinuationBootcamp()
+}
